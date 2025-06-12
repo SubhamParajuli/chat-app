ed59a8b900ca6d27e7e25a1bab2f200a1bdfca8d
Subham <suvamparajuli222@gmail.com>
2025-06-12 18:48:06 +0545

    Implement database connection and initialize on server start

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,16 @@
 import express from 'express';
 import 'dotenv/config';
 import authRoutes from "./routes/auth.route.js"
+import { connectDB } from './lib/db.js';
 
 
 const PORT=process.env.PORT
-
 const app=express()
 
 app.use("/api/auth",authRoutes)
 
 app.listen(PORT,()=>{
   console.log(`Server is Running on port ${PORT} `)
+  connectDB();
 })
 
